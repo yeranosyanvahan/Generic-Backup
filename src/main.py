727c2a8c6d8e8041d981c5f2d782dd5954c9f3e6
Vahan Yeranosyan <vyeranosyan@questrade.com>
2023-03-25 16:13:12 +0400

    add fixes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import docker
 import tarfile
 import io
+from datetime import datetime
 
 CONTAINER_NAME = "srv-mysql2-1"
 
@@ -10,12 +11,16 @@ container = client.containers.get(CONTAINER_NAME)
 
 output = container.exec_run('echo "hello world"').output.decode('utf-8')
 
+# Set the folder and filename for the tar file
+folder = datetime.now().strftime('%Y-%m')
+filename = datetime.now().strftime(f'%Y-%m-%d_{CONTAINER_NAME}.tgz')
+
 # Open the tar file
-with tarfile.open('output.tgz', 'w:gz') as tar:
-    # Add the output of the function to output.txt in the tar file
+with tarfile.open(f'{folder}/{filename}', 'w:gz') as tar:
+    # Add the output of the function to backup/backup.sql in the tar file
     output = "This is the output of my function"
     output_bytes = output.encode('utf-8')
-    tarinfo = tarfile.TarInfo('backup/output.txt')
+    tarinfo = tarfile.TarInfo('backup/backup.sql')
     tarinfo.size = len(output_bytes)
     tar.addfile(tarinfo, io.BytesIO(output_bytes))
 
