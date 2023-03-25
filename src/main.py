bdf9fd8a4c58d44f1b57ee692b7d093677a5393a
Vahan Yeranosyan <vyeranosyan@questrade.com>
2023-03-25 15:49:16 +0400

    fix

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import docker
 import tarfile
+import gzip
+import io
 
 CONTAINER_NAME="srv-mysql2-1"
 
@@ -9,9 +11,6 @@ container = client.containers.get(CONTAINER_NAME)
 
 output = container.exec_run('echo "hello world"').output.decode('utf-8')
 
-import tarfile
-import gzip
-
 # function that generates output
 def generate_output():
     return "This is the output of my function"
@@ -19,24 +18,27 @@ def generate_output():
 # open the existing tar file for writing
 with tarfile.open('output.tar', 'w:gz') as tf:
     
+    # create a TarInfo object for the output file
+    info = tarfile.TarInfo('output.gz')
+    output_bytes = output.encode('utf-8')
+    info.size = len(output_bytes)
+    
     # create a GzipFile object for writing compressed data
     with gzip.open('output.gz', 'wb') as gz:
         
         # write the output to the GzipFile
-        gz.write(output.encode('utf-8'))
-        
-        # create a TarInfo object for the output file
-        info = tarfile.TarInfo('output.gz')
-        info.size = len(output)
+        gz.write(output_bytes)
         
         # add the output file to the tar file
         tf.addfile(info, gz)
         
     # create a TarInfo object for the Readme file
     readme_info = tarfile.TarInfo('Readme.md')
-    readme_info.size = len('hello world')
+    readme_bytes = b'hello world'
+    readme_info.size = len(readme_bytes)
+    
+    # create a file-like object with the contents of the Readme file
+    readme_obj = io.BytesIO(readme_bytes)
     
     # add the Readme file to the tar file
-    with tf.extractfile(readme_info) as f:
-        f.write('hello world'.encode('utf-8'))
-        tf.addfile(readme_info, f)
+    tf.addfile(readme_info, readme_obj)
