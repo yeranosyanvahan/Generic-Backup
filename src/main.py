b073820fb29948f7add1a4d912cb5b907c3eec0c
Vahan Yeranosyan <vyeranosyan@questrade.com>
2023-03-25 16:39:02 +0400

    fix some things

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ config.read('cfg/main.ini')
 
 for section in config.sections():
     keys = {key:value for key, value in config.items(section)}
+    print(keys)
 
     if("BACKUPTYPE" not in keys):
         print("BACKUPTYPE is not present in main.ini")
